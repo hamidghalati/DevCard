2b1462869c880425fd940309e354b9010cc96be7
Hamid <Hamid@DESKTOP-NJ85KPP>
2022-05-14 12:51:20 +0430

    contact form validation implemented

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using DevCard_MVC.Models;
+﻿using System;
+using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Mime;
 
 
 namespace DevCard_MVC.Controllers
@@ -21,8 +23,29 @@ namespace DevCard_MVC.Controllers
 
         public IActionResult Contact()
         {
+            var model = new Contact();
             return View();
         }
+
+        [HttpPost]
+        public JsonResult Contact(Contact form)
+        {
+            Console.WriteLine(form.ToString());
+            return Json(Ok());
+        }
+
+        public FileResult test()
+        {
+            var fileByte = System.IO.File.ReadAllBytes("wwwroot/test.txt");
+            const string fileName = "testFile.txt";
+            return File(fileByte, MediaTypeNames.Text.Plain, fileName);
+        }
+
+        public IActionResult Result()
+        {
+            return new StatusCodeResult(401);
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DevCard_MVC/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace DevCard_MVC.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [MinLength(3,ErrorMessage = "حداقل طول نام 3 کاراکتر می باشد")]
+        [MaxLength(100,ErrorMessage = "حداکثر طول نام 100 کاراکتر می باشد")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [EmailAddress(ErrorMessage = "مقدار وارد شده ایمیل صحیح نیست")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [NotNull]
+        public string Message { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [NotNull]
+        public string Service { get; set; }
+    }
+}
